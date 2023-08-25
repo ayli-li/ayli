c7b9f106e5f9ee2a5e15636d8722c6798e474f34
Ilya Zezegov <zid@liis.su>
2023-08-25 11:24:13 +0300

    add translations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,29 @@
 import'./styles/index.scss'
-import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Component = () => {
+  
+}
 
 const App = () => {
   const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2'])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+      
     </div>
   )
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react'
 import cls from './Sidebar.module.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher'
 
 interface SidebarProps {
   className?: string
@@ -21,7 +22,7 @@ const Sidebar: FC<SidebarProps> = ({ className }) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* LangSwitcher */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   )
